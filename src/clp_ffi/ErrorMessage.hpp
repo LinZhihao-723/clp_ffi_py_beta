83eacb653bcb6664b0649deee077d51486c22926
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2023-06-03 00:40:57 -0400

    Add ErrorMessage.hpp

--- src/clp_ffi/ErrorMessage.hpp
+++ src/clp_ffi/ErrorMessage.hpp
@@ -1,12 +1,16 @@
-#ifndef CLP_FFI_PY_ERROR_MESSAGE
-#define CLP_FFI_PY_ERROR_MESSAGE
+#ifndef CLP_PY_ERROR_MESSAGE
+#define CLP_PY_ERROR_MESSAGE
 
-namespace clp_ffi_py::error_messages {
+namespace clp_ffi_py::ErrorMessage {
+constexpr char arg_parsing_error[] = "Native preamble encoder failed to parse Python arguments.";
+constexpr char out_of_memory_error[] = "Failed to allocate memory.";
+} // namespace clp_ffi_py::ErrorMessage
+
 namespace Encoding {
     constexpr char const* cTimestampError =
             "Native encoder cannot encode the given timestamp delta";
     constexpr char const* cPreambleError = "Native encoder cannot encode the given preamble";
     constexpr char const* cMessageError = "Native encoder cannot encode the given message";
 } // namespace Encoding
-} // namespace clp_ffi_py::error_messages
+
 #endif
