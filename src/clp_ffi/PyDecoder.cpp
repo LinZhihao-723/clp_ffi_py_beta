5febe01099f6ef25b0e915fe66043c0e10ffbed0
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2023-06-04 03:16:50 -0400

    Fix the problem. Instead of using the proxy, I directly register the buffer protocal callbacks.

--- src/clp_ffi/PyDecoder.cpp
+++ src/clp_ffi/PyDecoder.cpp
@@ -4,43 +4,6 @@
 #include <iostream>
 
 namespace clp_ffi_py::decoder {
-static int PyBufferProxy_getbuffer (PyObject* obj, Py_buffer* view, int flags) {
-    PyBufferProxy* self{reinterpret_cast<PyBufferProxy*>(obj)};
-    return PyBuffer_FillInfo(view, obj, self->data, self->size, 0, flags);
-}
-
-static PyBufferProcs PyBufferProxy_as_buffer = {
-        .bf_getbuffer = PyBufferProxy_getbuffer,
-};
-
-static void PyBufferProxy_dealloc (PyBufferProxy* self) {
-    // Since data is used as a proxy, we don't need to free the data region
-    Py_TYPE(self)->tp_free((PyObject*)self);
-}
-
-static PyTypeObject PyBufferProxyType = {
-        PyVarObject_HEAD_INIT(NULL, 0) "",        /* tp_name */
-        sizeof(PyBufferProxy),                    /* tp_basicsize */
-        0,                                        /* tp_itemsize */
-        (destructor)PyBufferProxy_dealloc,        /* tp_dealloc */
-        0,                                        /* tp_vectorcall_offset */
-        0,                                        /* tp_getattr */
-        0,                                        /* tp_setattr */
-        0,                                        /* tp_as_async */
-        0,                                        /* tp_repr */
-        0,                                        /* tp_as_number */
-        0,                                        /* tp_as_sequence */
-        0,                                        /* tp_as_mapping */
-        0,                                        /* tp_hash */
-        0,                                        /* tp_call */
-        0,                                        /* tp_str */
-        0,                                        /* tp_getattro */
-        0,                                        /* tp_setattro */
-        &PyBufferProxy_as_buffer,                 /* tp_as_buffer */
-        Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
-        "",                                       /* tp_doc */
-};
-
 PyObject* PyDecoderBuffer_new (PyTypeObject* type, PyObject* args, PyObject* kwds) {
     PyDecoderBuffer* self{reinterpret_cast<PyDecoderBuffer*>(type->tp_alloc(type, 0))};
     if (nullptr == self) {
@@ -58,19 +21,10 @@ PyObject* PyDecoderBuffer_new (PyTypeObject* type, PyObject* args, PyObject* kwd
     self->buf_size = 0;
     self->cursor_pos = 0;
 
-    self->proxy = reinterpret_cast<PyBufferProxy*>(PyObject_New(PyBufferProxy, &PyBufferProxyType));
-    if (nullptr == self->proxy) {
-        PyMem_Free(self->buf);
-        Py_TYPE(self)->tp_free(reinterpret_cast<PyObject*>(self));
-        PyErr_SetString(PyExc_RuntimeError, clp_ffi_py::ErrorMessage::out_of_memory_error);
-        Py_RETURN_NONE;
-    }
-
     return reinterpret_cast<PyObject*>(self);
 }
 
 void PyDecoderBuffer_dealloc (PyDecoderBuffer* self) {
-    Py_DECREF(self->proxy);
     PyMem_Free(self->buf);
     Py_TYPE(self)->tp_free(reinterpret_cast<PyObject*>(self));
 }
@@ -95,18 +49,9 @@ void PyDecoderBuffer::grow() {
     buf_size = num_unread_bytes;
 }
 
-PyBufferProxy* PyDecoderBuffer::get_proxy() {
-    assert(proxy);
-    assert(buf);
-    auto const num_remaining_bytes{buf_capacity - buf_size};
-    proxy->data = buf + buf_size;
-    proxy->size = num_remaining_bytes;
-    return proxy;
-}
-
 Py_ssize_t PyDecoderBuffer::read_into_buf(PyObject* istream) {
-    auto read_proxy{this->get_proxy()};
-    PyObject* retval = PyObject_CallMethod(istream, "readinto", "O", read_proxy);
+    PyObject* retval =
+            PyObject_CallMethod(istream, "readinto", "O", reinterpret_cast<PyObject*>(this));
     if (nullptr == retval) {
         PyErr_SetString(PyExc_RuntimeError, clp_ffi_py::ErrorMessage::return_error);
         return -1;
@@ -145,6 +90,18 @@ PyObject* PyDecoderBuffer_dump (PyDecoderBuffer* self) {
     Py_RETURN_NONE;
 }
 
+int PyDecoderBuffer_getbuffer (PyDecoderBuffer* self, Py_buffer* view, int flags) {
+    // Implement your getbuffer logic here
+    assert(self->buf);
+    auto const length{self->buf_capacity - self->buf_size};
+    auto const data{self->buf + self->buf_size};
+    return PyBuffer_FillInfo(view, reinterpret_cast<PyObject*>(self), data, length, 0, flags);
+}
+
+void PyDecoderBuffer_releasebuffer (PyDecoderBuffer* self, Py_buffer* view) {
+    // Doesn't do anything
+}
+
 static PyMethodDef PyDecoderBuffer_method_table[] = {
         {"read_from",
          reinterpret_cast<PyCFunction>(PyDecoderBuffer_read_from),
@@ -160,6 +117,8 @@ static PyType_Slot PyDecoderBuffer_slots[] = {
         {Py_tp_dealloc, reinterpret_cast<void*>(PyDecoderBuffer_dealloc)},
         {Py_tp_methods, PyDecoderBuffer_method_table},
         {Py_tp_init, nullptr},
+        {Py_bf_getbuffer, reinterpret_cast<void*>(PyDecoderBuffer_getbuffer)},
+        {Py_bf_releasebuffer, reinterpret_cast<void*>(PyDecoderBuffer_releasebuffer)},
         {Py_tp_new, reinterpret_cast<void*>(PyDecoderBuffer_new)},
         {0, nullptr}};
 
